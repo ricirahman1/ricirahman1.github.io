1c113ee25345769589e7ab5e046c7921bcbb5f47
ricirahman1 <rici7rahman@gmail.com>
2023-09-06 18:54:12 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+        center:true,
+        padding:'16px'
+    },
+    extend: {
+      backgroundImage: {
+        'image1': "url('/dist/img/image1.jpeg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      },
+      colors:{
+        primary:'#4338ca',
+        secondary:'#64748b',
+        dark:'#18181b',
+        gradient:'#1d2671'
+      },
+      screen:{
+        '2xl':'1320px'
+      }
+    },
+  },
+  plugins: [
+    
+  ],
+}
+
